c6a471d918d0e4876e4827ab8c56ee07378394a4
bryskulov <bakbergen3@gmail.com>
2020-03-06 14:46:03 +0600

    Add translations

--- config.py
+++ config.py
@@ -13,4 +13,5 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['brys.code@gmail.com']
-    POSTS_PER_PAGE = 3
+    POSTS_PER_PAGE = 25
+    LANGUAGES = ['en', 'es']
